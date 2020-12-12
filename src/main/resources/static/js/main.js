538902e62d6ac76bb413cf199ae32ef4c94e4e03
Hong Daepyo <h1o1n1g1@nate.com>
2020-12-13 05:00:02 +0900

    화면단 기능 구현 완료

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,11 +6,27 @@ var main = {
 		});
 		
 		document.getElementById("countrySelect").addEventListener("change", function(){
+			_this.clearField();
 			_this.countrySelect_change(this);
 		});
+		
+		document.getElementById("countrySelect").dispatchEvent(new Event("change"));
 	},
 	submitBtn_click : function(){
-		alert(1);
+		var amount = document.getElementById("sendAmount").value;
+		var rate = document.getElementById("exchangeRate").textContent;
+		rate = rate.replace(",", "");
+		
+		if (!this.checkSubmitValidation(amount)) {
+			this.setSubmitErrorMsg();
+			return;
+		}
+		
+		var receivedAmount = amount * rate;
+		var result = receivedAmount.toFixed(2).toString();
+		result = result.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		
+		this.setSubmitSuccessMsg(result);
 	},
 	countrySelect_change : function(elem){
 		this.getCurrency(elem.value);
@@ -22,7 +38,6 @@ var main = {
 			url: "/currency",
 			data: {country: country},
 			success: function(result){
-				console.log(result);
 				if (result.success) {
 					_this.setExchangeData(result);
 				} else {
@@ -37,6 +52,37 @@ var main = {
 	setExchangeData : function(data){
 		document.getElementById("exchangeRate").textContent = data.rate;
 		document.getElementById("exchangeRateUnit").textContent = data.toCountry + "/" + data.fromCountry;
+	},
+	checkSubmitValidation : function(amount){
+		var result = true;
+		
+		if(isNaN(amount)) { //숫자체크
+			result = false;
+		} else if (amount < 0 || amount > 10000) { //금액체크
+			result = false;
+		}
+		
+		return result;
+	},
+	setSubmitErrorMsg : function(){
+		var msg = "송금액이 바르지 않습니다";
+		document.getElementById("resultDiv").textContent = msg;
+		document.getElementById("resultDiv").className = "error";
+	},
+	setSubmitSuccessMsg : function(money){
+		var unit = document.getElementById("countrySelect").value;
+		var msg = "수취금액은 " + money + " " + unit + "입니다.";
+		
+		document.getElementById("resultDiv").textContent = msg;
+		document.getElementById("resultDiv").className = "success";
+	},
+	clearField : function(){
+		document.getElementById("resultDiv").textContent = "";
+		
+		document.getElementById("exchangeRate").textContent = "";
+		document.getElementById("exchangeRateUnit").textContent = "";
+		
+		document.getElementById("sendAmount").value = "";
 	}
 }
 
